af375cb5b617e9247e301754fdcac3d0017e3761
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-05 07:29:01 +0430

    Database information must insert at this file

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database setting,
+ * Put Your own information here:
+ */
+$db_info = array(
+    'name' => 'test',
+    'username' => 'root',
+    'password' => 'demo',
+    'host' => 'localhost',
+    'type' => 'mysql',
+    'port' => '3306'
+);
