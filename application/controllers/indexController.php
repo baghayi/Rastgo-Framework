2b088724ffb35c562828128cf85ed0975bfcd02a
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-07 00:27:33 +0430

    We just got rid of the extra files that we an example, and let it to have a simple method to show something as az example that how to use template engine and loadingModels

--- application/controllers/indexController.php
+++ application/controllers/indexController.php
@@ -3,36 +3,10 @@
 class indexController extends baseController {
     
     public function index() {
-//        $usersList = array();
-//        $contact = new mContact();
-//        while($res = $contact->contact()){
-//            $usersList[] =  $res['name'];
-//        }
-        
         View::renderTemplate(__FUNCTION__, array(
-            'title' => 'Main Title.',
-            'content' => 'The Main Page Of the MVC-Based Frameword!'
-//            'usersList' => $usersList
+            'title' => 'Framework\'s Main Title.',
+            'content' => 'The Main Page Of the MVC-Based Frameword!',
+            'modelmessage' => static::loadModel(true)
         ));
-        
-
     }
-
-    public function contact() {
-        echo 'Congratulation, You Are In the Contact Page';
-        echo '<br />';
-        var_dump(func_get_args());
-        echo '<br />';
-        
-        /**
-         * This bunch of code are just an example of calling and using the Model files
-         */
-        
-//        $contact = new mContact();
-//        while($res = $contact->contact()){
-//            echo $res['name'];
-//            echo '<br />';
-//        }
-    }
-
 }
