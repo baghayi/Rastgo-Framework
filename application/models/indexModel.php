3fb00777c04a9bbfbecf27f4e33c67b633b0d558
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-07 00:38:32 +0430

    A file as a model file created there as a class file and was added a method called index() and some lines of code az in example of model files to show how does it work

---
+++ application/models/indexModel.php
@@ -0,0 +1,9 @@
+<?php
+class indexModel extends baseModel {
+
+    public function index(){
+        $message = 'We Are Writing This Message From  Inside Of The Model File: => ' . __METHOD__;
+        return $message;
+    }
+
+}
