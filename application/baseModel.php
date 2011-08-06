703e55c79f250eb8012a997da559923207302c9b
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-07 00:25:27 +0430

    We just added an abstract method called index

--- application/baseModel.php
+++ application/baseModel.php
@@ -1,12 +1,14 @@
 <?php
-class baseModel extends PDO {
+abstract class baseModel extends PDO {
 
-    function __construct() {
+    public function __construct() {
         global $db_info;
         $dsn = $db_info['type'] . ':host=' . $db_info['host'] . ';port=' . $db_info['port'] . ';dbname=' . $db_info['name'];
         $username = $db_info['username'];
         $passwd = $db_info['password'];
         parent::__construct($dsn, $username, $passwd);
     }
+    
+    public abstract function index();
 
 }
