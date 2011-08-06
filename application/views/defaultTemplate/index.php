da1ccb7bbf631c3226fbe23409f4e2055ff48436
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-07 00:31:34 +0430

    Some changed for making it to load a new variable and got rid of other extra stuffs

--- application/views/defaultTemplate/index.php
+++ application/views/defaultTemplate/index.php
@@ -1,19 +1,14 @@
 <doceType>
     <html>
         <head>
-            <link rel="stylesheet" href="<?php echo TEMPLATE_SITE_PATH . 'stylesheets' . DS .'style.css'; ?>" type="text/css" /> 
+            <title><?=$title?></title>
+            <link rel="stylesheet" href="<?php echo SITE_TEMPLATE_PATH . 'stylesheets' . DS .'style.css'; ?>" type="text/css" /> 
         </head>
         <body>
-            <h2><?=$title?></h2>
-            <p><?=$content?></p>
-            
-<!--            <ol>
-                <?php //foreach($usersList as $user): ?>
-                <li><?=$user?></li>
-                <?php //endforeach; ?>
-            </ol>-->
-
+            <div id="maindiv">
+                <h2><?=isset($title)?$title:'';?></h2>
+                <p><?=isset($content)?$content:'';?></p>
+                <p><?=isset($modelmessage)?$modelmessage:'';?></p>
+            </div>
         </body>
-
-
     </html>
