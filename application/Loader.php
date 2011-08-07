51b9ddd1ea7608deb19252cc64204fabbee72507
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-07 05:03:27 +0430

    The autoloader file has been removed and its duty was gave to this file, In addition that this file (whole system) supports the namespace too

--- application/Loader.php
+++ application/Loader.php
@@ -1,26 +1,42 @@
 <?php
 final class Loader {
     protected $registry;
-    
-    function __construct(Registry $registry) {
+
+    function __construct(root\application\Registry\Registry $registry) {
         $this->registry = $registry;
     }
-    
-    public function loadModel($ModleName){
+
+    public function loadModel($ModleName) {
         $ModelCompleteName = $ModleName . 'Model';
         $ModelPath = FILE_PATH . 'application' . DS . 'models' . DS . $ModelCompleteName . '.php';
-        
-        if(file_exists($ModelPath)){
+
+        if (file_exists($ModelPath)) {
             require_once $ModelPath;
-            
-                if(class_exists($ModelCompleteName))
-                    $this->registry->model = new $ModelCompleteName;
-                
-                else
-                    throw new Exception('Model Class Does Not Exists!');
-                
-        }else{
+
+            if (class_exists($ModelCompleteName))
+                $this->registry->model = new $ModelCompleteName;
+
+            else
+                throw new Exception('Model Class Does Not Exists!');
+        }else {
             throw new Exception('Model File Could Not Be Found!');
+        }
+    }
+
+    public static function setAutoLoader() {
+        spl_autoload_register(array(__class__, 'autoLoader'));
+    }
+
+    public static function autoLoader($namespace) {
+
+        $filePath = explode('\\', $namespace);
+        $className = array_pop($filePath);
+        array_shift($filePath);
+        $filePath = implode(DS, $filePath);
+        $filePath = FILE_PATH . $filePath . '.php';
+
+        if (file_exists($filePath)) {
+            require_once $filePath;
         }
     }
 
