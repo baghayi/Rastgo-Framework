8246603e7a58ef32d3cae0acc578ab3bd5e0bed3
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-07 05:15:05 +0430

    The way of extending the baseModel has changed, its namespase id added

--- application/models/indexModel.php
+++ application/models/indexModel.php
@@ -1,9 +1,9 @@
 <?php
-class indexModel extends baseModel {
+class indexModel extends \root\application\baseModel\baseModel {
 
     public function index(){
         $message = 'We Are Writing This Message From  Inside Of The Model File: => ' . __METHOD__;
         return $message;
     }
-
+    
 }
