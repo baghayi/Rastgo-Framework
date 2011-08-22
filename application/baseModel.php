3788e09c34e917b554ee2970e3bf81d07f30d16b
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-23 03:53:01 +0430

    In this class the only thing that have has been changed is the way of calling the catabase information to use it for PDO class

--- application/baseModel.php
+++ application/baseModel.php
@@ -2,9 +2,11 @@
 abstract class baseModel extends PDO {
 
     public function __construct() {
-        $dsn = DatabaseConfig::getDBType() . ':host=' . DatabaseConfig::getDBHost() . ';port=' . DatabaseConfig::getDBPort() . ';dbname=' . DatabaseConfig::getDBName();
-        $username = DatabaseConfig::getDBUsername();
-        $passwd = DatabaseConfig::getDBPassword();
+        global $registry;
+        
+        $dsn = $registry->db->getDBType() . ':host=' . $registry->db->getDBHost() . ';port=' . $registry->db->getDBPort() . ';dbname=' . $registry->db->getDBName();
+        $username = $registry->db->getDBUsername();
+        $passwd = $registry->db->getDBPassword();
         parent::__construct($dsn, $username, $passwd);
     }
     
