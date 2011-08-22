d5fc082f1f4736c9fdb4548f8b11c33ba811be70
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-23 03:33:36 +0430

    We do not insert registry instance to the class and instead of that we have used global work to access registry's instance

--- application/baseController.php
+++ application/baseController.php
@@ -1,16 +1,16 @@
 <?php
 abstract class baseController {
-    public static $registry;
     
-    public function __construct(\root\application\Registry\Registry $registy) {
-        static::$registry = $registy;
-        static::$registry->view = new \root\application\View\View();
+    public function __construct() {
+        global $registry;
+        $registry->view = new \root\application\View\View();
     }
     
     protected static function loadModel($loadMethod = false){
-        static::$registry->loader->loadModel(static::$registry->request->getController());
+        global $registry;
+        $registry->loader->loadModel($registry->request->getController());
         if($loadMethod === TRUE)
-            return static::$registry->model->{static::$registry->request->getMethod()}();
+            return $registry->model->{$registry->request->getMethod()}();
     }
 
     public abstract function index();
