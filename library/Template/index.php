c1c6cc198a07487ef1e7b3703bd58c0b11828c79
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-24 18:47:07 +0430

    We have changed the class from being a full static class to an ordinary class, for ease of use and to decrease the server usage

--- library/Template/index.php
+++ library/Template/index.php
@@ -6,43 +6,43 @@ class Template {
      *  gives us the direction of template files where they are located
      * @var string $baseDir
      */
-    private static $baseDir = '.';
+    private $baseDir = '.';
     /**
      *  The extension of template files that we are using
      * @var string $defaultTemplateExtension
      */
-    private static $defaultTemplateExtension = '.php';
+    private $defaultTemplateExtension = '.php';
 
     /**
      *
      * @param string $dir to set or change the location of template files folder where template files are located
      */
-    public static function setBaseDir($dir) {
-        self::$baseDir = $dir;
+    public function setBaseDir($dir) {
+        $this->baseDir = $dir;
     }
 
     /**
      *
      * @return string gives us the template files folder where they are located 
      */
-    public static function getBaseDir() {
-        return self::$baseDir;
+    public function getBaseDir() {
+        return $this->baseDir;
     }
 
     /**
      *
      * @param string $ext To set or change the extention of template files
      */
-    public static function setExtension($ext) {
-        self::$defaultTemplateExtension = $ext;
+    public function setExtension($ext) {
+        $this->defaultTemplateExtension = $ext;
     }
 
     /**
      *
      * @return string Gives us the extention of template files
      */
-    public static function getExtension() {
-        return self::$defaultTemplateExtension;
+    public function getExtension() {
+        return $this->defaultTemplateExtension;
     }
 
     /**
@@ -52,17 +52,17 @@ class Template {
      * @param string $baseDir The folder of template files where they are located
      * @return string returns the rendered template file 
      */
-    public static function loadTemplate($template, $vars = array(), $baseDir=null) {
+    public function loadTemplate($template, $vars = array(), $baseDir=null) {
         if ($baseDir == null) {
-            $baseDir = self::getBaseDir();
+            $baseDir = $this->getBaseDir();
         }
 
-        $templatePath = $baseDir . '/' . $template . '' . self::getExtension();
+        $templatePath = $baseDir . '/' . $template . '' . $this->getExtension();
         if (!file_exists($templatePath)) {
             \root\library\ErrorReporting\index\ErrorReporting::reportError('Could not include template ' . $templatePath, __LINE__, __METHOD__, true);
         }
 
-        return self::loadTemplateFile($templatePath, $vars);
+        return $this->loadTemplateFile($templatePath, $vars);
     }
 
     /**
@@ -71,8 +71,8 @@ class Template {
      * @param array $vars The name and value of variables in template file to replace them with our values
      * @param string $baseDir The folder of template files where they are located
      */
-    public static function renderTemplate($template, $vars = array(), $baseDir=null) {
-        echo self::loadTemplate($template, $vars, $baseDir);
+    public function renderTemplate($template, $vars = array(), $baseDir=null) {
+        echo $this->loadTemplate($template, $vars, $baseDir);
     }
 
     /**
@@ -81,7 +81,7 @@ class Template {
      * @param array $vars The name and value of variables in template file to replace them with our values
      * @return string Returns the rendered template files as result 
      */
-    private static function loadTemplateFile($templatePath, $vars) {
+    private function loadTemplateFile($templatePath, $vars) {
         extract($vars, EXTR_OVERWRITE);
         $template_return = '';
         ob_start();
