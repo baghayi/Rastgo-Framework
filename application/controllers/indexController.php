c15e163c30fd6cf97fa2f97c8cd98226f6a3a99a
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-24 19:00:00 +0430

    The way of calling the view (our template class) hass been changed instead of calling its method in static mood

--- application/controllers/indexController.php
+++ application/controllers/indexController.php
@@ -2,7 +2,9 @@
 class indexController extends baseController {
     
     public function index() {
-        \root\application\View\View::renderTemplate(__FUNCTION__, array(
+        global $registry;
+        
+        $registry->view->renderTemplate(__FUNCTION__, array(
             'title' => 'Framework\'s Main Title.',
             'content' => 'The Main Page Of the MVC-Based Frameword!',
             'modelmessage' => static::loadModel(true)
