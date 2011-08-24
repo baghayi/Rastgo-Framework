66b198ca2c973fab28927150a0b912bd989dbe63
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-08-24 18:50:00 +0430

    This class is an empty class now, and just exdends from the Template class and nothing more

--- application/View.php
+++ application/View.php
@@ -2,8 +2,4 @@
 namespace root\application\View;
 class View extends \root\library\Template\index\Template {
     
-    public function __construct() {
-        \root\library\Template\index\Template::setBaseDir(TEMPLATE_DIR_ADDRESS);
-    }
-    
 }
