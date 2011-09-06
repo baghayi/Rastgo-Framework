dc200b5a4dc1bae11a3909c83ab9c41f5fcf8f4f
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-06 06:55:24 +0430

    CACHE_MAIN_DIR constant was fixed, the DS constant was added to it

--- bootStrap.php
+++ bootStrap.php
@@ -46,7 +46,7 @@ define('LOG_FOLDER_PATH', FILE_PATH . 'logs' . DS);
 /**
  * Cache main folder path,
  */
-define('CACHE_MAIN_DIR', FILE_PATH . 'cache' . DS . 'cachefiles');
+define('CACHE_MAIN_DIR', FILE_PATH . 'cache' . DS . 'cachefiles' . DS);
  
 /**
  * We are just including the Loader file that can been able to load or include other classes and files,
