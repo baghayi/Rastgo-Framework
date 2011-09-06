303beb7b484deb6f775faf092762212d9eca3d0d
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-06 07:15:23 +0430

    A method just to show how cache class works

--- application/controllers/indexController.php
+++ application/controllers/indexController.php
@@ -10,4 +10,28 @@ class indexController extends baseController {
             'modelmessage' => static::loadModel(true)
         ));
     }
+    
+    public function cache(){
+        global $registry;
+        $registry->cache = new root\library\Cache\index\Cache;
+        $registry->cache->setHashFileName(false);
+        
+        /**
+         * If the Cache file exists these bunch of codes will be excuted
+         */
+        if($registry->cache->getCache('cache','cache')){
+            echo $registry->cache->getCache('cache','cache');
+            return;
+        }
+        /**
+         * If the cache file does not exists then these belowe codes will be excuted
+         */
+        $registry->cache->startBuffer();
+        $registry->view->renderTemplate('index', array(
+            'title' => 'Framework\'s Main Title.',
+            'content' => 'The Main Page Of the MVC-Based Frameword!',
+            'modelmessage' => static::loadModel(true)
+        ));
+        echo $registry->cache->cacheBuffer('cache','cache',3);
+    }
 }
