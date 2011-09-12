f68adbaddbd6c0c3a0f2977120c5b9ef60670edc
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-12 19:47:42 +0430

    Some chages in its appearance and the php end tag was added

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ $registry = \root\application\Registry\Registry::getInstance();
 $registry->error = new \root\library\ErrorReporting\index\ErrorReporting;
 $registry->loader = new Loader;
 $registry->request = new \root\application\Request\Request();
-try{
+
+try {
     $registry->db = new DatabaseConfig($dbConfig);
     $registry->router = new \root\application\Router\Router();
-}catch(Exception $e){
+} catch (Exception $e) {
     echo $e->getMessage();
 }
+?>
