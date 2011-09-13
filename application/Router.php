42c526045df24d9653d547605f4c5326a81316d2
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-14 02:45:04 +0430

    Some changes to ease of reading for developers, and had no other changes

--- application/Router.php
+++ application/Router.php
@@ -29,9 +29,11 @@ class Router {
         require_once FILE_PATH . 'application' . DS . 'baseController.php';
         require_once $this->controllerAddress;
         $this->controllerInstance = new $this->controllerName();
+        
         if(is_a($this->controllerInstance, $this->controllerName)){
             return 1;
         }
+        
         $registry->error->reportError('Controller Counld Not Be instantiated.', __LINE__, __METHOD__, true);
         return;
     }
@@ -43,6 +45,7 @@ class Router {
             $registry->error->reportError('Entered Method ( '. $registry->request->getMethod() .' ) Cound Not Be Found', __LINE__, __METHOD__, true);
             return;
         }
+        
         return 1;
     }
     
@@ -50,11 +53,12 @@ class Router {
         global $registry;
         
         $args = $registry->request->getArgs();
+        
         if(empty($args)){
-            call_user_func(array($this->controllerName,$registry->request->getMethod()));
+            call_user_func(array($this->controllerName, $registry->request->getMethod()));
             return 1;
         }else{
-            call_user_func_array(array($this->controllerName,$registry->request->getMethod()), $registry->request->getArgs());
+            call_user_func_array(array($this->controllerName, $registry->request->getMethod()), $registry->request->getArgs());
             return 1;
         }
     }
