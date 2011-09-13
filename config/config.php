a3dbedbdc20648c9344f41a5ecbb293e77575daa
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-14 02:53:16 +0430

    Some changes to ease of reading the codes for developers

--- config/config.php
+++ config/config.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * If the script is running in the root then do not fill it up,
- * But if it is in a folder put here that folder name ( DO NOT USE 'FORWARD SLASH'=> / NOR 'BACK SLASH'=> \ )
+ * If the script is running in the root then do NOT fill it up.
+ * But if it is in a folder put here that folder name ( DO NOT USE 'FORWARD SLASH'=> / NOR 'BACK SLASH'=> \ ) .
  */
-define('SCRIPT_ROOT_FOLDER_NAME','RastgoFramework');
+define('SCRIPT_ROOT_FOLDER_NAME', 'RastgoFramework');
 
 /**
- * Web site protocol that are useing, like (http, https)
+ * Web site protocol that are useing, like (http, https) .
  */
-define('WEB_PROTOCOL','http');
+define('WEB_PROTOCOL', 'http');
 
 /**
- * The Template Folder Name, 
+ * The Template Folder Name.
  */
-define('TEMPLATE_FOLDER_NAME','defaultTemplate');
+define('TEMPLATE_FOLDER_NAME', 'defaultTemplate');
 
