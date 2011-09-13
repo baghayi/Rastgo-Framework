8a3005e99451d3eefe475a8c908fc6cfec4f7824
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-14 02:57:08 +0430

    The loader class are calling with its namespace that had defined

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'bootStrap.php';
 
 $registry = \root\application\Registry\Registry::getInstance();
 $registry->error = new \root\library\ErrorReporting\index\ErrorReporting;
-$registry->loader = new Loader;
+$registry->loader = new \root\application\Loader\Loader;
 $registry->request = new \root\application\Request\Request();
 
 try {
