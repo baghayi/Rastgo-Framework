9bd23ae7a8681def4b77cde07829c69d00e120f6
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-14 02:36:27 +0430

    Comment are grammatically fixed and some changes (adding spaces) for ease of reading the code

--- library/ErrorReporting/index.php
+++ library/ErrorReporting/index.php
@@ -3,22 +3,22 @@ namespace root\library\ErrorReporting\index;
 
 final class ErrorReporting {
     /**
-     * With this property we want to seperate the error messages in files instead of putting all error messages toghether in a one file,
-     * And this array are going to be equaled with one parameter of the reportError() method,
+     * With this property we want to seperate the error messages in files instead of putting all error messages toghether in a one file.
+     * And this array are going to be equaled with one parameter of the reportError() method.
      * @errorTypes array
      */
     private $errorTypes = array('database', 'authentication', 'others');
-    private $logFileExtention = '.txt',$logDirPath;
+    private $logFileExtention = '.txt', $logDirPath;
     /**
-     *  This property is going to contain the complete error, includes error line, error method, can cantain IP, the main message, ...
-     *  To log it and or even throw it as a exception
+     *  This property is going to contain the complete error, includes error line, error method, can cantain IP, the main message, ... .
+     *  To log it and or even throw it as a exception.
      * @reportedError string 
      */
     private $reportedError;
     /**
-     * The size of the each log files, A limitation for Log Files
-     * The number is in byte,
-     * And by default it's 10 M or 10000000 bytes
+     * The size of the each log files, A limitation for Log Files.
+     * The number is in byte.
+     * And by default it's 10 M or 10000000 bytes .
      * @var integer  $logFileSize
      */
     private $logFileSize = 10000000;
@@ -29,8 +29,8 @@ final class ErrorReporting {
     }
 
     /**
-     *  This is the main method that we can report errors with it,
-     *  And then the class will do the other thing itself
+     *  This is the main method that we can report errors with it.
+     *  And then the class will do the other thing itself.
      * @param string $message
      * @param integer $line
      * @param string $methodName
@@ -44,9 +44,12 @@ final class ErrorReporting {
         $this->reportedError = "- {$TodaysDate_Time} | {$userIPAddress} | Error Message: {$message}, At line: {$line}, In Method: {$methodName}, The Referer Address: {$referedPlace} . \r\n";
         $this->spliteLogFile($errorType);
         $this->WriteInLogFile($errorType);
-        if ($throwException == TRUE)
-            $this->throwException($message);
         
+        if ($throwException == TRUE){
+            $this->throwException($message);
+            return 1;
+        }
+
         return 1;
     }
 
@@ -56,36 +59,41 @@ final class ErrorReporting {
     }
 
     /**
-     * With this method we are wrting the error message in the log file
+     * With this method we are wrting the error message in the log file.
      * @param string $fileName 
      */
     private function WriteInLogFile($fileName) {
         $this->createLogDir();
-        $this->createLogFiles();
-        $fileAddress = $this->logDirPath .$fileName . DS . $fileName . $this->logFileExtention;
+        $this->createLogFolders();
+        $fileAddress = $this->logDirPath . $fileName . DS . $fileName . $this->logFileExtention;
+        
         if (!($handle = fopen($fileAddress, 'a'))) {
             $this->throwException('The Log File Is Not Able To Be Opened!');
             return;
         }
+        
         if (FALSE === fwrite($handle, $this->reportedError)){
             $this->throwException('Reported Message Could Not Be Written In The Log File!');
             return;
         }
+        
         fclose($handle);
         return 1;
     }
 
     /**
-     * With this method we are creating the Log files,
+     * With this method we are creating the Log Folders, To be sure they are exists or not.
      */
-    private function createLogFiles() {
+    private function createLogFolders() {
+        
         foreach ($this->errorTypes as $fileName) {
-            /**
-             * Checing to create directories,
-             */
+            
             if(!file_exists($this->logDirPath . $fileName) or !is_dir($this->logDirPath . $fileName)){
-                if(is_writable($this->logDirPath))
+                
+                if(is_writable($this->logDirPath)){
                     mkdir($this->logDirPath . $fileName);
+                }
+                
                 else{
                     $this->throwException('The Log Directory Is Not Writable, Please Change Its Permission To 777 And Then Refresh The Page,<br /> If You Did Not Get Any Messages Like This Again, Change That File\'s Permission to 755, <br /> Directory Address: <strong> ' . $this->logDirPath . ' </strong>');
                     return;
@@ -101,7 +109,7 @@ final class ErrorReporting {
     }
 
     /**
-     * This method can add new error types to ErrorTypes array
+     * This method can add new error types to ErrorTypes array.
      * Error Types are file names that reported errors will be stored in them!
      * @param string $errorType 
      */
@@ -111,7 +119,7 @@ final class ErrorReporting {
     }
 
     /**
-     * This methid can return us the Error types (file names) whicj errors will be stored in them, as an array
+     * This methid can return us the Error types (file names) whicj errors will be stored in them, as an array.
      * @return array
      */
     public function showErrorTypes() {
@@ -119,20 +127,23 @@ final class ErrorReporting {
     }
 
     private function spliteLogFile($fileName) {
-        if(file_exists($fileAbsolutePath = $this->logDirPath . $fileName .DS .$fileName. $this->logFileExtention)) {
-            $fileNewAbsolutePath = $this->logDirPath . $fileName .DS. strftime("%Y_%m_%d_%H_%M_%S", time()) . '__' . $fileName . $this->logFileExtention;
+        
+        if(file_exists($fileAbsolutePath = $this->logDirPath . $fileName . DS . $fileName . $this->logFileExtention)) {
+            $fileNewAbsolutePath = $this->logDirPath . $fileName . DS . strftime("%Y_%m_%d_%H_%M_%S", time()) . '__' . $fileName . $this->logFileExtention;
+            
             if (filesize($fileAbsolutePath) >= $this->logFileSize) {
+                
                 if (false === rename($fileAbsolutePath, $fileNewAbsolutePath)){
                     $this->reportError('The Log File Could Not Be Renamed!', __LINE__, __METHOD__,false);
                     return;
-                }   
+                }
             }
         }
         return 1;
     }
     
     /**
-     *  With this method we are able to set/change the file limitation size,
+     *  With this method we are able to set/change the file limitation size.
      *  Remember that it must be an integer and it must be in bytes!
      * @param integer $fileSize 
      */
@@ -142,12 +153,16 @@ final class ErrorReporting {
     }
     
     private function createLogDir(){
+        
         if(!file_exists($this->logDirPath) || !is_dir($this->logDirPath)){
             chdir('__rfolder');
+            
             if(is_writable(dirname($this->logDirPath))){
                 mkdir($this->logDirPath);
                 return 1;
-            }else{
+            }
+            
+            else{
                 $this->throwException('The __rfolder Directory Is Not Writable, Please Change Its Permission To 777 And Then Refresh The Page,<br /> If You Did Not Get Any Messages Like This Again, Change That File\'s Permission to 755, <br /> Directory Address: <strong> ' . dirname($this->logDirPath) . ' </strong>');
                     return;
             }
