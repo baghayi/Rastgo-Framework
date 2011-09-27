c01a1fed85482f3148ec2d025f282105bb09f8cc
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-09-27 06:57:35 +0330

    a back slash addedd befor PDO name when its extending, and a namespae addedd to the class, and also a parameter addedd to the construct method just to running a query that soves unicode languages problesm that its not running by default

--- application/baseModel.php
+++ application/baseModel.php
@@ -1,13 +1,21 @@
 <?php
-abstract class baseModel extends PDO {
+namespace root\application\baseModel;
 
-    public function __construct() {
+abstract class baseModel extends \PDO {
+
+    public function __construct($unicodeQuery = false) {
         global $registry;
         
         $dsn = $registry->db->getDBDriver() . ':host=' . $registry->db->getDBHost() . ';port=' . $registry->db->getDBPort() . ';dbname=' . $registry->db->getDBName();
         $username = $registry->db->getDBUsername();
         $passwd = $registry->db->getDBPassword();
         parent::__construct($dsn, $username, $passwd);
+        
+        if($unicodeQuery === true)
+        {
+            $sth = $this->prepare('SET NAMES utf8');
+            $sth->execute();
+        }
         return;
     }
     
