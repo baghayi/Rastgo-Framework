2e7f8739034232a9c36025631a830f04fbafe88d
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-11-27 02:36:45 +0330

    A new option added to the file as DB configuration array key (pdoOptions) that is used for the fourth paramter of the PDO class and descriptions added to the each variable of the file to explain that are they doing

--- config/database.php
+++ config/database.php
@@ -4,9 +4,39 @@
  * Put Your own information here:
  */
 static $dbConfig = array();
+
+/**
+ * The name of the database (required).
+ */
 $dbConfig['name'] = 'test';
+
+/**
+ * The username of the database (required).
+ */
 $dbConfig['username'] = 'root';
+
+/**
+ * The password of the database (required).
+ */
 $dbConfig['password'] = 'demo';
+
+/**
+ * The host name of the database (required).
+ */
 $dbConfig['host'] = '127.0.0.1';
+
+/**
+ * The driver name of the database (required).
+ */
 $dbConfig['driver'] = 'mysql';
-$dbConfig['port'] = 3306;
+
+/**
+ * The port number of the database (optional).
+ */
+//$dbConfig['port'] = 3306;
+
+/**
+ * The variable will be given to the fourth parameter of the PDO class as options (optional).
+ * And must be an array.
+ */
+#$dbConfig['pdoOptions'] = array();
