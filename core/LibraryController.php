46b7c5d201857e033446c47a7021bc1bd428b4c3
Hossein Baghayi <hossein.baghayi@gmail.com>
2011-12-31 04:36:32 +0330

    This class will try to load defined classes in the config/libraryGlobalizing.ini file and then instantiate it and make it's object accessible through the Registry class property that user defined in the config file

--- core/LibraryController.php
+++ core/LibraryController.php
@@ -6,14 +6,14 @@ namespace root\core\LibraryController
     final class LibraryController implements iLibraryController
     {
         
-        public function call($libraryName, $constructorsArgument = array()) 
+        public static function call($libraryName, $constructorsArgument = array()) 
         {
-            if(!$this->libraryExistence($libraryName))
+            if(!self::libraryExistence($libraryName))
                 return 0;
 
-            $classAddress =  $this->classAddress($libraryName);
+            $classAddress =  self::classAddress($libraryName);
             
-            if($this->methodExistence($classAddress, '__construct'))
+            if(self::methodExistence($classAddress, '__construct'))
             {
                 $reflectionObject = new \ReflectionClass($classAddress);
                 $object = $reflectionObject->newInstanceArgs($constructorsArgument);
@@ -28,22 +28,82 @@ namespace root\core\LibraryController
             return 0;
         }
         
-        public function methodExistence($libraryName, $methodName)
+        public static function methodExistence($libraryName, $methodName)
         {
             if(!method_exists($libraryName, $methodName))
                 return 0;
             return 1;
         }
         
-        public function libraryExistence($libraryName)
+        public static function libraryExistence($libraryName)
         {
-            return class_exists($this->classAddress($libraryName));
+            return class_exists(self::classAddress($libraryName));
         }
         
-        public function classAddress($libraryName)
+        public static function classAddress($libraryName)
         {
-            return str_replace('*', $libraryName, LibraryController::libraryNamespace);
+            return str_replace('*', $libraryName, self::libraryNamespace);
         }
         
+        public static function configFileAddress()
+        {
+            return (FILE_PATH . 'config' . DS . self::configFileName);
+        }
+        
+        public static function parseConfigFile()
+        {
+            if(!file_exists(self::configFileAddress()))
+            {
+                global $registry;
+                $registry->error->reportError("(" . self::configFileName . ") file is not found at this direction: (". self::configFileAddress(). ")", __LINE__, __METHOD__, false);
+                return 0;
+            }
+            
+            return parse_ini_file(self::configFileAddress(), true);
+        }
+        
+        public static function determineArguments($parsedFileResult)
+        {
+            $count = 1;
+            $finalresultAsArray = array();
+            
+            foreach($parsedFileResult as $key => $value)
+            {
+                if($key == ($res = 'Argument:' . $count))
+                {
+                    foreach($value as $secondKey => $secondValue)
+                    {
+                        $finalresultAsArray[$secondKey][] = $secondValue;
+                    }
+                    $count++;
+                }
+                
+            }
+            return $finalresultAsArray;
+        }
+        
+        public static function globalizeObject()
+        {
+            $settings = self::parseConfigFile();
+            $argumentsAsArray = array();
+            global $registry;
+            
+            $arguments = self::determineArguments($settings);
+
+            foreach($settings['Properties'] as $properties => $values)
+            {
+                if(isset($arguments[$properties]))
+                {
+                    $argumentsAsArray = $arguments[$properties];
+                }
+                
+                $object = self::call($values, $argumentsAsArray);
+                
+                if(is_a($object, self::classAddress($values)))
+                    $registry->{$properties} = $object;
+            }
+            
+            return new LibraryController;
+        }
     }
 }
