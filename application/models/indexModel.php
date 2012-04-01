30d7a5034aeb6515b4d8e7fc34fe5b8f37dd16a6
Hossein Baghayi <hossein.baghayi@gmail.com>
2012-04-01 23:53:14 +0430

    The way of injection Dependency to this class is changed and it's Dependency classes are injected using the contructor method that it's dependency is Just Registry class, and also descriptions added to it's methods. and aslo there were some changes about calling the requested controller and it's methods (action) that now it is using the Reflection classes to do that.

--- application/models/indexModel.php
+++ application/models/indexModel.php
@@ -1,5 +1,5 @@
 <?php
-class indexModel extends \root\core\baseModel\baseModel {
+class indexModele extends \root\core\baseModel\baseModel {
 
     public function index(){
         return 'We Are Writing This Message From  Inside Of The Model File: => ' . __METHOD__;
--- core/Router.php
+++ core/Router.php
@@ -1,76 +1,86 @@
 <?php
 namespace root\core\Router;
 
-class Router 
+class Router
 {
-    private $controllerInstance, $controllerName, $controllerAddress;
-    
-    public function __construct() {
+    private $controllerReflectionInstance,
+            $controllerInstance,
+            $controllerName,
+            $controllerAddress,
+            $registry;
+
+    /**
+     * Using this constructor method we inject the Object of the Registry class and then calling other methods of the class!
+     * @param \root\core\Registry\Registry $registry Object of the class Registry.
+     */
+    public function __construct(\root\core\Registry\Registry $registry)
+    {
+        $this->registry = $registry;
+
         $this->getController();
         $this->instantiatingController();
         $this->checkingMethod();
         $this->callingMethod();
+
         return;
     }
-    
-    private function getController(){
-        global $registry;
+
+    /**
+     * This method make the name and address of the controller ready and check to see if there is any controller like what is called and then setting them to their proper properties.
+     * In addition that, if the called controller does not exist, user will be redirected to the error controller in it's Controller nofFound methid (action) with the oarameter that it's value is 'Controller'.
+     * @return void
+     */
+    private function getController()
+    {
+        $this->controllerAddress = FILE_PATH . 'application' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . $this->registry->request->getController() . 'Controller.php';
         
-        $controllerAddress = FILE_PATH . 'application' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . $registry->request->getController() . 'Controller.php';
-        
-        if(file_exists($controllerAddress) and is_readable($controllerAddress))
+        if(file_exists($this->controllerAddress) && is_readable($this->controllerAddress))
         {
-            $this->controllerName = $registry->request->getController() . 'Controller';
-            $this->controllerAddress = $controllerAddress;
-            return 1;
+            $this->controllerName = $this->registry->request->getController() . 'Controller';
+            return;
         }
 
         header("Location: " . URL . 'error/notFound/Controller/');
         exit;
     }
-    
-    private function instantiatingController(){
-        global $registry;
+
+    /**
+     * At this part (method) we instantiate the called controller using the Reflection class and saving that object in it's propper property.
+     * @return void
+     */
+    private function instantiatingController()
+    {
         require_once $this->controllerAddress;
-        $this->controllerInstance = new $this->controllerName();
-        
-        if(is_a($this->controllerInstance, $this->controllerName)){
-            return 1;
-        }
-        
-        $registry->error->reportError('Controller Counld Not Be instantiated.', __LINE__, __METHOD__, true);
+        $this->controllerReflectionInstance = new \ReflectionClass($this->controllerName);
+        $this->controllerInstance = $this->controllerReflectionInstance->newInstance($this->registry->getInstance());
+
         return;
     }
-    
-    private function checkingMethod(){
-        global $registry;
-        
-        if(!in_array($registry->request->getMethod(), get_class_methods($this->controllerName)))
+
+    /**
+     * This method checks to see if there is requested method (action) and it is public, otherwise user will be redirected to the controller of error and it's method of notFound with the parameter of the "Method" (the value of parameter).
+     * @return void
+     */
+    private function checkingMethod()
+    {
+        if(
+            !($this->controllerReflectionInstance->hasMethod($this->registry->request->getMethod())) ||
+            !($this->controllerReflectionInstance->getMethod($this->registry->request->getMethod())->isPublic()))
         {
             header("Location: " . URL . 'error/notFound/Method/');
             exit;
         }
-        
-        if(!method_exists($this->controllerName, $registry->request->getMethod()))
-        {
-            header("Location: " . URL . 'error/notFound/Method/');
-            exit;
-        }
-        
-        return 1;
-    }
-    
-    private function callingMethod(){
-        global $registry;
-        $args = $registry->request->getArgs();
-        
-        if(empty($args)){
-            call_user_func(array(new $this->controllerName, $registry->request->getMethod()));
-            return 1;
-        }else{
-            call_user_func_array(array(new $this->controllerName, $registry->request->getMethod()), $registry->request->getArgs());
-            return 1;
-        }
+
+        return;
     }
 
+    /**
+     * Now, we are ready to invoke the requested method (action) and also pass the arguments to that method.
+     * @return void
+     */
+    private function callingMethod()
+    {
+        $this->controllerReflectionInstance->getMethod($this->registry->request->getMethod())->invoke($this->controllerInstance, $this->registry->request->getArgs());
+        return;
+    }
 }
