6b72c613386c694f45b89a228f4aceb26a3135f3
Hossein Baghayi <hossein.baghayi@gmail.com>
2012-04-24 16:18:31 +0430

    The Registry class in injected to the class instead of calling it via global keyword,
    And also loadModel is changed from being an static class to an non-static class.
    And also that a property with the name of $registry is added to this class for storing the Object of the Registry class.

--- core/baseController.php
+++ core/baseController.php
@@ -2,39 +2,43 @@
 namespace root\core\baseController;
 
 abstract class baseController {
-    
-    public function __construct() {
-        global $registry;
-        $registry->view = new \root\core\View\View();
+
+    public $registry = NULL;
+
+    public function __construct(\root\core\Registry\Registry $registry) {
+        $this->registry = $registry;
+        $this->registry->view = new \root\core\View\View();
         return;
     }
     
-    protected static function loadModel($loadDefaultMethod = false, $modelName = NULL, $modelMethod = NULL){
-        global $registry;
-        
-        if($modelName !== NULL){
+    protected function loadModel($loadDefaultMethod = false, $modelName = NULL, $modelMethod = NULL)
+    {
+        if($modelName !== NULL)
+        {
             $modelToBeCalled = $modelName;
         }
         else{
-            $modelToBeCalled = $registry->request->getController();
+            $modelToBeCalled = $this->registry->request->getController();
         }
         
-        if($modelMethod !== NULL){
+        if($modelMethod !== NULL)
+        {
             $modelMethodCalled = $modelMethod;
         }
         else{
-            $modelMethodCalled = $registry->request->getMethod();
+            $modelMethodCalled = $this->registry->request->getMethod();
         }
             
-        $registry->loader->loadModel($modelToBeCalled);
+        $this->registry->loader->loadModel($modelToBeCalled);
         
-        if($loadDefaultMethod === TRUE){
-            
-            if(method_exists($modelToBeCalled.'Model', $modelMethodCalled)){
-                return $registry->model->{$modelMethodCalled}();
+        if($loadDefaultMethod === TRUE)
+        {
+            if(method_exists($modelToBeCalled.'Model', $modelMethodCalled))
+            {
+                return $this->registry->model->{$modelMethodCalled}();
             }
             else{
-                $registry->error->reportError('Requested Method Via The Controller Does Not Exists In The Model File.', __LINE__, __METHOD__,true);
+                $this->registry->error->reportError('Requested Method Via The Controller Does Not Exists In The Model File.', __LINE__, __METHOD__,true);
                 return;
             }
         }
