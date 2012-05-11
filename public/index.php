85fe23b506a47c030054ece6afdbef24ae6c83fd
Hossein Baghayi <hossein.baghayi@gmail.com>
2012-05-11 18:14:49 +0430

    this method Loader::setAutoLoader is being called and object of the registry class is injected to this method, then we will have access to the object of the Registry class from inside of the Loader object.

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 require_once dirname(dirname(__FILE__)) . '/core/bootStrap.php';
-
 $registry = \root\core\Registry\Registry::getInstance();
+\root\core\Loader\Loader::setAutoLoader($registry);
 
-    try 
+    try
     {
         $registry->lib = \root\core\LibraryController\LibraryController::globalizeObject();
         $registry->loader = new \root\core\Loader\Loader;
         $registry->request = new \root\core\Request\Request();
         $registry->router = new \root\core\Router\Router();
-    } 
-    catch (Exception $e) 
+    }
+    catch (Exception $e)
     {
         echo $e->getMessage();
     }
