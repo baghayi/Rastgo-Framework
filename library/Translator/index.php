8b9c74ef1fd3a349c837d7b50276cb2ba79207e8
Hossein Baghayi <hossein.baghayi@gmail.com>
2012-05-11 18:19:23 +0430

    A new public and static property added to this class named $registry to keep the object of the registry class and then use it rather than using global keyword to have access to the object of the Registry class.

--- library/Translator/index.php
+++ library/Translator/index.php
@@ -1,21 +1,25 @@
 <?php
 namespace root\library\Translator\index;
 
-final class Translator {
-    
+final class Translator
+{
+    public static $registry = NULL;
     private $defaultLanguage ='english', $langFileExtension = '.php', $languageArray = array();
     
-    public function setDefaultLang($language){
+    public function setDefaultLang($language)
+    {
         $this->defaultLanguage = $language;
         return 1;
     }
     
-    public function setLangFileExtension($extension){
+    public function setLangFileExtension($extension)
+    {
         $this->langFileExtension = $extension;
         return 1;
     }
     
-    public function translate($keyWord, $fileName = null){
+    public function translate($keyWord, $fileName = null)
+    {
         
         if($fileName !== null){
              $this->loadLanguageFile($fileName);
@@ -24,15 +28,16 @@ final class Translator {
         return isset($this->languageArray[$keyWord])?$this->languageArray[$keyWord]:null;
     }
     
-    public function loadLanguageFile($fileName){
-        global $registry;
+    public function loadLanguageFile($fileName)
+    {
         $fileAddress = FILE_PATH . 'application' . DIRECTORY_SEPARATOR . '__rfolder' . DIRECTORY_SEPARATOR . 'languages' . DIRECTORY_SEPARATOR . $this->defaultLanguage . DIRECTORY_SEPARATOR . $fileName . $this->langFileExtension;
         
         if(file_exists($fileAddress)){
             $this->languageArray = require_once $fileAddress;
             
-            if(!is_array($this->languageArray)){
-                $registry->error->reportError('The Returned Value From The Called Language File Is Not An Array!', __LINE__, __METHOD__, true);
+            if(!is_array($this->languageArray))
+            {
+                self::$registry->error->reportError('The Returned Value From The Called Language File Is Not An Array!', __LINE__, __METHOD__, true);
                 return;
             }
             
@@ -40,7 +45,7 @@ final class Translator {
         }
         
         else{
-            $registry->error->reportError('Wanted Language Package (File) Does Not Exists!', __LINE__, __METHOD__, true);
+            self::$registry->error->reportError('Wanted Language Package (File) Does Not Exists!', __LINE__, __METHOD__, true);
             return;
         }
     }
