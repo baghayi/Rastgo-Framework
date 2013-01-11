89b4182cae4f578ca347e43c28c8499954ccfb95
Hossein Baghayi <hossein.baghayi@gmail.com>
2013-01-11 15:44:46 +0330

    A new static and public property name $registry added to this core class so that we can have access to the object of Registry class in our template files without needing registry class to be injected to template files.

--- core/View.php
+++ core/View.php
@@ -3,5 +3,10 @@ namespace root\core\View;
 
 class View extends \root\library\Template\index\Template
 {
+    /**
+     * This property will store the object of the Registry class so we can have access to the object of the Registry class inside of out templates.
+     * @var object
+     */
+    public static $registry;
     
 }
