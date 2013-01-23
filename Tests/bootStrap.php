954b66ffdd0ac2d7e8d74e4f64457b4ecef12257
Hossein Baghayi <hossein.baghayi@gmail.com>
2013-01-23 15:43:05 +0330

    xdebug_disable is being called in this file to prevent generating long error messages, because it is not needed in testing.

--- Tests/bootStrap.php
+++ Tests/bootStrap.php
@@ -1,3 +1,10 @@
 <?php
+/**
+ * This code checks to see if xdebug_disable function exists or not, if so then will run it to disable xdebug.
+ * As its known, Xdebug will generate long error messages that is not necessary in testing and rather having a line of error message which can indicate the root of problem will do.
+ */
+if(function_exists('xdebug_disable'))
+    xdebug_disable();
+
 //this line includes composer autoloader file. then this file (bootStrap) will be used by phpunit to map the RastgoFramework classes.
 require_once 'vendor/autoload.php';
