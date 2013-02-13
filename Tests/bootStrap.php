2826f96702f3adea5615fe9e5a3123b2bb0f3acb
Hossein Baghayi <hossein.baghayi@gmail.com>
2013-02-14 01:29:32 +0330

    Loading bootStrap file of the framework which some constant were defined in it were required to use them in testing some libraries that were coded not properly (hard coded) so by refactoring and loose coupling that the issue solved and this file is no longer required.

--- Tests/bootStrap.php
+++ Tests/bootStrap.php
@@ -1,6 +1,4 @@
 <?php
-require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'bootStrap.php';
-
 /**
  * This code checks to see if xdebug_disable function exists or not, if so then will run it to disable xdebug.
  * As its known, Xdebug will generate long error messages that is not necessary in testing and rather having a line of error message which can indicate the root of problem will do.
